02caf04b5ef952b1975b08156c1f7c350d741704
Jan Hoon <janhoon97@gmail.com>
2025-05-03 17:39:55 +0100

    feat: style and layout improvements

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import ParseTreeVisualizer from "@/components/parse-tree-visualizer";
 import SQLEditor from "@/components/sql-editor";
-import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
-} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
-import { GithubIcon, HomeIcon } from "lucide-react";
+import { GithubIcon, HomeIcon, InfoIcon } from "lucide-react";
 import type { Node } from "web-tree-sitter";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "./components/ui/popover";
 
 // Sample SQL queries for examples
 const EXAMPLE_QUERIES = [
@@ -51,7 +50,9 @@ FROM mydb.myschema.users AS u;
 ];
 
 function App() {
-	const [sqlQuery, setSqlQuery] = useState<string>(EXAMPLE_QUERIES[0].query);
+	const [sqlQuery, setSqlQuery] = useState<string>(
+		"-- Load an exmample query above or create your own\nSELECT current_date",
+	);
 	const [hoveredNode, setHoveredNode] = useState<Node | null>(null);
 
 	// Handle SQL query changes
@@ -67,33 +68,37 @@ function App() {
 	return (
 		<>
 			<main className="flex h-screen max-h-screen flex-col w-screen antialiased bg-gray-800 text-gray-100">
-				<header className="border-b border-gray-600 p-4 flex felx-row">
+				<header className="p-4 pb-7 flex felx-row">
 					<div className="w-full gap-3 flex flex-col">
-						<h1 className="text-2xl font-bold">CADAC SQL Previewer</h1>
-						<p>
-							The SQL we create tells us a lot about the data we are planning on
-							fetching. Using tools like{" "}
-							<a
-								href="https://github.com/janhoon/tree-sitter-sql"
-								className="text-blue-500 hover:underline"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<span className="text-emerald-400 hover:underline">
-									Tree Sitter Parsers
-								</span>
-							</a>{" "}
-							we can analyze the structure and semantics of SQL queries. Hover
-							over the tree nodes to see how the query is structured and how the
-							different parts of the query are related to each other.
-						</p>
-						<Accordion type="single" collapsible>
-							<AccordionItem value="more-info">
-								<AccordionTrigger className="!bg-gray-800">
-									More Info
-								</AccordionTrigger>
-								<AccordionContent className="p-0 m-0">
+						<h1 className="text-2xl font-bold flex flex-row items-start gap-2">
+							CADAC SQL Previewer{" "}
+							<Popover>
+								<PopoverTrigger>
+									<InfoIcon className="hover:text-emerald-700 hover:cursor-pointer" />
+								</PopoverTrigger>
+								<PopoverContent
+									className="bg-gray-900 text-gray-200 w-1/3 border-gray-600"
+									side="right"
+								>
 									<div className="flex flex-col gap-2 p-5">
+										<p>
+											The SQL we create tells us a lot about the data we are
+											planning on fetching. Using tools like{" "}
+											<a
+												href="https://github.com/janhoon/tree-sitter-sql"
+												className="text-blue-500 hover:underline"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<span className="text-emerald-400 hover:underline">
+													Tree Sitter Parsers
+												</span>
+											</a>{" "}
+											we can analyze the structure and semantics of SQL queries.
+											Hover over the tree nodes to see how the query is
+											structured and how the different parts of the query are
+											related to each other.
+										</p>
 										<p>
 											Using the editor below you can create your own SQL queries
 											and preview the results. The Resulting tree structure
@@ -130,39 +135,44 @@ function App() {
 											</a>
 										</div>
 									</div>
-								</AccordionContent>
-							</AccordionItem>
-						</Accordion>
+								</PopoverContent>
+							</Popover>
+						</h1>
 					</div>
 					<a href="https://janhoon.com">
 						<HomeIcon className="mr-2 text-white hover:text-green-700" />
 					</a>
 				</header>
 
-				<div className="flex flex-row gap-2 justify-start items-center w-full border-b border-gray-600 py-1">
-					<h2 className="mr-2 text-sm font-medium">Examples:</h2>
-					{EXAMPLE_QUERIES.map((query) => (
-						<Button
-							key={query.name}
-							onClick={() => loadExample(query.name)}
-							className="!bg-gray-900"
-						>
-							{query.name}
-						</Button>
-					))}
+				<div className="flex flex-col gap-2 w-full border-b border-gray-600 pt-2 pb-4">
+					<h2 className="ml-4 mr-2 text-lg font-bold">Load Example:</h2>
+					<div className="flex flex-row gap-2">
+						{EXAMPLE_QUERIES.map((query, index) => (
+							<Button
+								variant="default"
+								key={query.name}
+								onClick={() => loadExample(query.name)}
+								className="bg-gray-800 hover:text-emerald-600 hover:bg-gray-900 w-[100px]"
+							>
+								{`${query.name}`}
+							</Button>
+						))}
+					</div>
 				</div>
 
 				<div className="flex flex-col sm:flex-row flex-1 bg-gray-900 overflow-auto">
-					{/* SQL Editor (Right) */}
+					{/* SQL Editor */}
 					<div className="w-full h-[300px] sm:min-h-min sm:h-full">
+						<h2 className="m-3 text-2xl">SQL Editor</h2>
 						<SQLEditor
 							value={sqlQuery}
 							onChange={handleSqlChange}
 							hoveredNode={hoveredNode}
 						/>
 					</div>
-					<div className="w-full">
-						{/* Parse Tree Visualization (Left) */}
+					{/* Parse Tree Visualization */}
+					<div className="w-full border-l border-gray-700">
+						<h2 className="m-3 text-2xl pt-12 sm:pt-0">Parser</h2>
 						<ParseTreeVisualizer
 							sqlQuery={sqlQuery}
 							onHoverNode={setHoveredNode}
